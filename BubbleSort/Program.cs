6b4ee4d12525e5a2710cfc4ee5794fa6238a8922
ice19942335 <ice19942335@gmail.com>
2020-03-26 13:11:10 +0000

    Add project files.

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            int[] arr = new int[20];
+            int temp;
+
+            for (int i = 0; i < arr.Length; i++)
+                arr[i] = rnd.Next(1, 100);
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (arr[j] > arr[i])
+                    {
+                        temp = arr[j];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+
+                PrintArray(arr);
+            }
+        }
+
+        private static void PrintArray(int[] arr)
+        {
+            Console.Clear();
+
+            for (int i = 0; i < arr.Length; i++)
+                Console.Write(arr[i] + " ");
+
+            Thread.Sleep(100);
+        }
+    }
+}
